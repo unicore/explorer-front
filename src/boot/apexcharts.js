b87a545d443c8da64ecce5948e52c6b5dcdba75c
Dark Sun <dacom.dark.sun@gmail.com>
2023-03-14 16:51:07 +0300

    smaller

--- src/boot/apexcharts.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import VueApexCharts from 'vue-dapexcharts'
-
-// import VueDApexCharts from 'vue-dapexcharts'
-Vue.use(VueApexCharts)
-
-Vue.component('apexchart', VueApexCharts)
-// Vue.component('dapexchart', VueDApexCharts)
--- src/boot/apollo.js
+++ src/boot/apollo.js
@@ -1,36 +1,3 @@
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import VueApollo from 'vue-apollo'
-import fetch from 'node-fetch'
-import { createHttpLink } from 'apollo-link-http'
-import config from '../config'
-
-const httpLink = createHttpLink({ uri: config.graphql, fetch: fetch })
-
-// Create the apollo client
-const apolloClient = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
-  connectToDevTools: true
-})
-
-export const apolloProvider = new VueApollo({
-  defaultClient: apolloClient,
-  errorHandler ({ graphQLErrors, networkError }) {
-    if (graphQLErrors) {
-      graphQLErrors.map(({ message, locations, path }) =>
-        console.log(
-          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-        )
-      )
-    }
-    if (networkError) {
-      console.log(`[Network error]: ${networkError}`)
-    }
-  }
-})
-
 export default ({ app, Vue }) => {
-  Vue.use(VueApollo)
-  app.apolloProvider = apolloProvider
+  
 }
